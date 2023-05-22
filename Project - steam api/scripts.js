5bd679ec472638b69b2ec87f1b3f86a19cfb7862
Oskar Migowski <o.migowski@protonmail.com>
2023-05-22 07:48:00 +0200

    changed project from steam to nasa api

--- Project - steam api/scripts.js
+++ Project - steam api/scripts.js
@@ -5,7 +5,7 @@ window.onload = function() {
     var loginButton = document.getElementById("loginButton");
     var nicknameElement = document.querySelector(".user-nickname");
     var profilePicElement = document.querySelector(".profile-pic");
-    const secretValue = process.env.STEAM_API_KEY;
+    const secretValue = "B94010C95F9D0B6D23F9A7DC3AC23BD1";
 
     btn.onclick = function() {
         modal.style.display = "block";
@@ -26,7 +26,7 @@ window.onload = function() {
         e.preventDefault(); // Stop the form from submitting normally
 
         var id = document.getElementById("id").value;
-        var url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${encodeURIComponent(secretValue)}&steamids=${id}`;
+        var url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${secretValue}&steamids=${id}`;
         var proxyUrl = 'http://omigo.me:8888/';
 
         var statusElement = document.querySelector(".status");
@@ -46,8 +46,6 @@ window.onload = function() {
             })
             .then(data => {
                 var userInfo = data.response.players[0];
-                var nicknameElement = document.querySelector(".user-nickname");
-                var profilePicElement = document.querySelector(".profile-pic");
     
                 nicknameElement.innerText = userInfo.personaname;
                 profilePicElement.src = userInfo.avatar;
@@ -87,5 +85,71 @@ window.onload = function() {
         // Update the UI with stored ID
         document.getElementById("id").value = storedId;
         loginButton.textContent = "Log out";
+
+        calculateTotalSpent(storedId, secretValue);
+        calculateInventoryWorth(storedId, secretValue);
     }
+}
+
+function calculateTotalSpent(id, secretValue) {
+    var gamesUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${secretValue}&steamid=${id}&include_appinfo=1&include_played_free_games=1`;
+    var gamePricesUrlBase = 'http://store.steampowered.com/api/appdetails?appids=';
+    var proxyUrl = 'http://omigo.me:8888/';
+
+    // Fetch the list of games the user owns
+    fetch(proxyUrl + gamesUrl)
+      .then(response => response.json())
+      .then(data => {
+        var games = data.response.games;
+        var totalWorth = 0;
+
+        // Iterate over each game
+        games.forEach(game => {
+          // Fetch the current price of the game
+          var gamePricesUrl = gamePricesUrlBase + game.appid;
+          fetch(proxyUrl + gamePricesUrl)
+            .then(response => response.json())
+            .then(gameData => {
+              var price = gameData[game.appid].data.price_overview.final;
+
+              totalWorth += price;
+
+              // Update UI with total worth
+              document.getElementById('gamesWorth').innerText = `Games worth: ${totalWorth}`;
+            });
+        });
+      })
+      .catch(error => console.error('Error:', error));
+}
+
+function calculateInventoryWorth(id, secretValue) {
+    var inventoryUrl = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${secretValue}&steamid=${id}&include_appinfo=1&include_played_free_games=1`;
+    var itemPricesUrlBase = 'http://api.steampowered.com/ISteamEconomy/GetAssetPrices/v0001?'; // Base URL for fetching item prices
+    var proxyUrl = 'http://omigo.me:8888/';
+
+    // Fetch the user's inventory
+    fetch(proxyUrl + inventoryUrl)
+      .then(response => response.json())
+      .then(data => {
+        var items = data.response.games;
+        var totalWorth = 0;
+
+        // Iterate over each item in the user's inventory
+        items.forEach(item => {
+          // Fetch the current price of the item
+          var itemPricesUrl = itemPricesUrlBase + 'appid=' + item.appid;
+          fetch(proxyUrl + itemPricesUrl)
+            .then(response => response.json())
+            .then(itemData => {
+              // Assuming 'prices' is the correct field name, this will need to be updated according to the actual response structure
+              var price = itemData.result.prices[item.appid].price;
+
+              totalWorth += price;
+
+              // Update UI with total worth
+              document.getElementById('inventoryWorth').innerText = `Inventory worth: ${totalWorth}`;
+            })
+        });
+      })
+      .catch(error => console.error('Error:', error));
 }
